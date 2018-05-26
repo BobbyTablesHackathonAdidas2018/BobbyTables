8bd69c6074d7605b245fd168c6eb073afcc48fda
furstenheim <furstenheim@geoblink.com>
2018-05-26 14:34:05 +0200

    Get song

--- backend/bpmService.js
+++ backend/bpmService.js
@@ -2,12 +2,16 @@ const request = require('request')
 const bpmKey = 'bca4d13a3b1505181f0d029fcacea404'
 const bpmUrl = 'https://api.getsongbpm.com/search/'
 const urlJoin = require('url-join')
-const url = require('url')
+const url = require('url') 
 const querystring = require('querystring')
 const puppeteer = require('puppeteer')
-getNextSong()
-
+// getNextSong(100, 'Metallica')
+module.exports = getNextSong
 async function getNextSong (bpm, seedSong) {
+  let returnResolve
+  const returnPromise = new Promise(function (resolve, reject) {
+    returnResolve = resolve
+  })
   let browser, page
   try {
     browser = await puppeteer.launch({
@@ -17,38 +21,42 @@ async function getNextSong (bpm, seedSong) {
       browser.close()
     })
     const page = await browser.newPage()
-    await page.focus('div')
     page.on('error', function (err) {
       console.error('Global error page', err)
     })
     page.on('response', async function (response) {
       try {
-        const json = await response.json()
-        console.log(json)
+        let json
+        try {
+          json = await response.json()
+        } catch (e) {
+
+        }
+        if (response.request().url().startsWith('http://www.songkeybpm.com/Advanced/AdvancedQuery')) {
+          returnResolve(json.trackItems[0])
+        }
       } catch (e) {
+        console.log(e)
         // console.log('request is not json')
       }
     })
 
     await page.goto('http://www.songkeybpm.com/Advanced')
+    await page.type('input.advanced-seed-search-field', seedSong)
+    await page.click('input.advanced-seed-search-field')
+    await page.keyboard.press(String.fromCharCode(13))
 
+    await page.waitForSelector('.searchedSeedsList .detailed-album-container')
+    await page.click('.searchedSeedsList .detailed-album-container')
 
+    await page.waitForSelector('.selectedSeedNode')
+    await page.click('#advancedSearchInputContainer-BPM')
+    await page.type('#primaryField-BPM', '' + bpm)
+    await page.click('.as-search-button')
+
+    return returnPromise
   } catch (e) {
     console.error('Error on main', e)
   }
 
- /* const a = await new Promise(function (resolve, reject) {
-    request.get(bpmUrl, {
-      qs: {
-      api_key: bpmKey,
-        type: 'song',
-      lookup: 'Metallica'
-    }
-  }, function (err, response) {
-      if (err) return reject(err)
-      resolve(response)
-    })
-  })*/
-
-  console.log(a)
 }
